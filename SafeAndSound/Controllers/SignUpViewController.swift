83e4add71bf252e6de5e012ceb1193f88e7dc843
Brandon Teran <brandonteran@aim.com>
2018-10-13 10:37:23 -0700

    removed some view controller files

--- SafeAndSound/Controllers/SignUpViewController.swift
+++ SafeAndSound/Controllers/SignUpViewController.swift
@@ -50,16 +50,4 @@ class SignUpViewController: UIViewController {
         tap.cancelsTouchesInView = false
         self.view.addGestureRecognizer(tap)
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
